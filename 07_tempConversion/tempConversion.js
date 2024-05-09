c8e9a01147123a690433d0b02784f5a913bf44e6
chriswebdv <chriswebdv@gmail.com>
2024-05-09 17:54:40 -0600

    Create function that convert celsius to farenheit and vice versa rounding to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+// °C = (°F - 32) × 5/9
+// °F = (9/5 × °C) + 32.
+
+const convertToCelsius = function(temp) {
+  temp = ((temp - 32) * 5/9).toFixed(1);
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (((9/5) * temp) + 32).toFixed(1);
+  return temp;
 };
+
+console.log(convertToCelsius(89));
+console.log(convertToFahrenheit(22));
 
 // Do not edit below this line
 module.exports = {
