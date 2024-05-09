e139258b33ecb492487524736943145879f6be33
chriswebdv <chriswebdv@gmail.com>
2024-05-09 17:03:03 -0600

    Create function that add all numbers inside of a range using a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (num1 < 0 || isNaN(num1)){
+        return "ERROR";
+    } else if (num2 < 0 || isNaN(num2)){
+        return "ERROR";
+    } 
+
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
+
 
 };
+
+console.log(sumAll(2, 6));
 
 // Do not edit below this line
 module.exports = sumAll;
